b158d6df760168cf0b9e40d0fab176b37c49b449
thejagger <jackpot.auer5@gmail.com>
2025-03-25 00:50:11 +0100

    new privacy policy added

--- lib/pages/settings/privacy_policy.dart
+++ lib/pages/settings/privacy_policy.dart
@@ -1,31 +1,22 @@
 import 'package:deun/l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:markdown_widget/widget/markdown.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class PrivacyPolicy extends StatelessWidget {
   const PrivacyPolicy({super.key});
 
   @override
   Widget build(BuildContext context) {
+    WebViewController controller = WebViewController()..loadRequest(Uri.parse('https://deun.app/privacy_policy'));
+
     return Scaffold(
       appBar: AppBar(
         title: Text(AppLocalizations.of(context)!.settingsPrivacyPolicy),
       ),
       body: Padding(
-        padding: EdgeInsets.only(left: 10, right: 10),
-        child: FutureBuilder(
-          future: rootBundle.loadString('assets/markdown/privacy_policy.md'),
-          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-            if (snapshot.hasData) {
-              return MarkdownWidget(
-                data: snapshot.data!,
-              );
-            }
-
-            return const Center(child: CircularProgressIndicator());
-          },
-        ),
+        padding: EdgeInsets.only(bottom: 30),
+        child: WebViewWidget(controller: controller),
       ),
     );
   }
